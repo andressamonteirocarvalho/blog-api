2b3905fb361751102b384ff1e4a66b50ddb8658f
Andressa Monteiro <andressa18monteiro@gmail.com>
2022-10-31 19:19:40 -0300

    Criação da Classe Referencias e seus atributos

---
+++ Referencias.java
@@ -0,0 +1,29 @@
+public class Referencias {
+    private int id;
+    private String nome;
+    private String titulo;
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setNome( String nome){
+        this.nome = nome; 
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setTitulo( String titulo){
+        this.titulo = titulo; 
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+}
